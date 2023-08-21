cb48c628a882d3216abdbf62e12f98769e392c0a
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2023-08-21 16:34:45 +0800

    Create whitelisted.lua

---
+++ whitelisted.lua
@@ -0,0 +1,24 @@
+--[[
+This is an example of a ModuleScript :nerd:, loadstring it and get values in return.
+
+commands (user is the prefix, similar to "default" in vape private):
+;kick user
+;anticheat user (kick)
+;lagback user
+;crash user
+;backrooms user
+;unbackrooms user
+;void user
+;fling user
+;funnyfling user (renders character funi)
+;kill user
+
+]]
+
+local whitelisted = {}
+
+whitelisted = {
+    ["uwuARUNTIKcringe"] = 4949002127,
+}
+
+return whitelisted
